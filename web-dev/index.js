4f736795f319dff48764c701321320e33f929767
anandsuralkar <90561527+anandsuralkar@users.noreply.github.com>
2021-10-26 07:39:46 +0530

    added some code in index.html and index.js

--- web-dev/index.js
+++ web-dev/index.js
@@ -1,1 +1,17 @@
-window.alert("Hello World!")
+var nums=[1,2,3,4,5];
+let string="My name is anand";
+// console.log(string.slice(2,7));
+//console.log(string.replace("anand","rohan"));
+// let mydate=new Date();
+// console.log(mydate.getTime());
+// console.log(mydate.getFullYear());
+// console.log(mydate.getHours());
+// console.log(mydate.getMinutes());
+// console.log(mydate.getDay());
+// let elem =  document.getElementById('click');
+// console.log(elem);
+
+let elemClass = document.getElementById('container');
+console.log(elemClass[0]);
+
+// elemClass.classList.add('text color');
